84923c1fd744ca95a22f966975e95e420e2e8aec
Kevin5920 <kevin.ruvalcaba5920@alumnos.udg.mx>
2024-04-22 15:19:01 -0600

    Update main.py

--- main.py
+++ main.py
@@ -28,3 +28,33 @@ def santa_sleeps():
 
 def santa():
     global required_reindeers, required_elves, elves_counter, reindeer_counter
+    print("Santa Claus")
+    while True:
+        santa_semaphore.acquire()
+        mutex.acquire()
+        if reindeer_counter == required_reindeers:
+            prepare_sleigh()
+            for _ in range(required_reindeers):
+                reindeer_counter -= 1
+                reindeer_semaphore.release()
+                sleep(randint(1, 2))
+        elif elves_counter == required_elves:
+            help_elves()
+            for _ in range(required_elves):
+                elves_counter -= 1
+                sleep(randint(1, 2))
+        mutex.release()
+        santa_sleeps()
+
+
+def elves():
+    global elves_counter, required_elves
+    while True:
+        elves_mutex.acquire()
+        mutex.acquire()
+        elves_counter += 1
+        if elves_counter == required_elves:
+            santa_semaphore.release()
+        else:
+            elves_mutex.release()
+        mutex.release()
