023b266b1caf83def0a7df020c2e04c4909dfb7c
Athziri Rodríguez <avanessa.rodriguez@alumnos.udg.mx>
2024-04-22 15:08:20 -0600

    Add santa sleeps method, remaining imports and variables

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from threading import Semaphore
 from threading import Thread
-import time
+from time import sleep
+from random import randint
 
+required_elves = 3
+required_reindeers = 9
 elves_counter = 0
 reindeer_counter = 0
 santa_semaphore = Semaphore()
-reindeer_semaphore = Semaphore()
-mutex = Semaphore(1)
+reindeer_semaphore = Semaphore(required_reindeers)
+elf_semaphore = Semaphore()
+elves_mutex = Semaphore()
+mutex = Semaphore()
 
 
 def prepare_sleigh():
@@ -17,23 +22,9 @@ def help_elves():
     print("Helping elves")
 
 
+def santa_sleeps():
+    print("Santa goes to sleep")
+
+
 def santa():
-    global elves_counter, reindeer_counter
-    print("Santa Claus")
-    while True:
-        santa_semaphore.acquire()
-        mutex.acquire()
-        if reindeer_counter >= 9:
-            prepare_sleigh()
-            for _ in range(9):
-                reindeer_semaphore.release()
-            print("Santa Claus delivers gifts")
-            reindeer_counter -= 9
-            time.sleep(5)
-        elif elves_counter == 3:
-            help_elves()
-        mutex.release()
-
-
-elf_threads = []
-reindeer_thread = []
+    global required_reindeers, required_elves, elves_counter, reindeer_counter
