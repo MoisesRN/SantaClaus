18230db942ca4852e91fa425a02717c94f47b2e7
Jimmycc7 <89420110+Jimmycc7@users.noreply.github.com>
2024-04-29 12:54:58 -0600

    Front funcionando

--- main.py
+++ main.py
@@ -1,31 +1,28 @@
 from threading import Semaphore, Thread
-from time import sleep, time
-from random import randint
+from time import sleep
 import tkinter as tk
 from PIL import Image, ImageTk
+import random
 
 required_elves = 3
 required_reindeers = 9
 elves_counter = 0
 reindeer_counter = 0
+
 santa_semaphore = Semaphore()
 reindeer_semaphore = Semaphore(required_reindeers)
 elf_semaphore = Semaphore()
 elves_mutex = Semaphore()
 mutex = Semaphore()
 
-
 def prepare_sleigh():
     print("Preparing sleigh")
-
 
 def help_elves():
     print("Helping elves")
 
-
 def santa_sleeps():
     print("Santa goes to sleep")
-
 
 def santa():
     global required_reindeers, required_elves, elves_counter, reindeer_counter
@@ -38,15 +35,14 @@ def santa():
             for _ in range(required_reindeers):
                 reindeer_counter -= 1
                 reindeer_semaphore.release()
-                sleep(randint(1, 2))
+                sleep(random.randint(1, 2))
         elif elves_counter == required_elves:
             help_elves()
             for _ in range(required_elves):
                 elves_counter -= 1
-                sleep(randint(1, 2))
+                sleep(random.randint(1, 2))
         mutex.release()
         santa_sleeps()
-
 
 def elves():
     global elves_counter, required_elves
@@ -60,12 +56,11 @@ def elves():
             elves_mutex.release()
         mutex.release()
         print(f"Elf {elves_counter}")
-        sleep(randint(1, 2))
+        sleep(random.randint(1, 2))
         mutex.acquire()
         if not elves_counter:
             elves_mutex.release()
         mutex.release()
-
 
 def reindeers():
     global reindeer_counter, required_reindeers
@@ -77,8 +72,7 @@ def reindeers():
         mutex.release()
         print(f"Reindeer {reindeer_counter}")
         reindeer_semaphore.acquire()
-        sleep(randint(1, 2))
-
+        sleep(random.randint(1, 2))
 
 def run_program():
     santa_thread = Thread(target=santa)
@@ -89,79 +83,76 @@ def run_program():
     elf_threads.start()
     reindeer_threads.start()
 
-
-santa_image = Image.open("resources/Recurso2.png")
-santa_sleep_image = Image.open("resources/Recurso1.png")
-elf_image = Image.open("resources/Recurso4.png")
-reindeer_image = Image.open("resources/Recurso3.png")
-
-rs_santa_image = santa_image.resize((200,200))
-rs_santa_sleep_image = santa_sleep_image.resize((200,200))
-rs_elf_image = elf_image.resize((50,50))
-rs_reindeer_image = reindeer_image.resize((50,50))
-
-
-
 def main():
+    global elves_counter, reindeer_counter
+
     # Crear ventana
     window = tk.Tk()
     window.title("North Pole")
     window.geometry("720x720")
 
     # Cargar imágenes
+    santa_image1 = Image.open("resources/Recurso1.png")
+    santa_image2 = Image.open("resources/Recurso2.png")
+    elf_image = Image.open("resources/Recurso4.png")
+    reindeer_image = Image.open("resources/Recurso3.png")
 
-    # Crear marcos para los bloques de imágenes
+    rs_santa_image1 = santa_image1.resize((200, 200))
+    rs_santa_image2 = santa_image2.resize((200, 200))
+    rs_elf_image = elf_image.resize((50, 50))
+    rs_reindeer_image = reindeer_image.resize((50, 50))
+
+    santa_photo1 = ImageTk.PhotoImage(rs_santa_image1)
+    santa_photo2 = ImageTk.PhotoImage(rs_santa_image2)
+    elf_photo = ImageTk.PhotoImage(rs_elf_image)
+    reindeer_photo = ImageTk.PhotoImage(rs_reindeer_image)
+
     elves_frame = tk.Frame(window)
     reindeers_frame = tk.Frame(window)
     santa_frame = tk.Frame(window)
 
-    # Organizar los marcos verticalmente
-    elves_frame.pack(side="left",  padx=50)
-    reindeers_frame.pack(side="left",  padx=50)
-    santa_frame.pack(side="left",  padx=50)
-    
-    santa_photo = ImageTk.PhotoImage(rs_santa_image)
+    elves_frame.pack(side="left", padx=50)
+    reindeers_frame.pack(side="left", padx=50)
+    santa_frame.pack(side="left", padx=50)
 
-    
-    elf_photo = ImageTk.PhotoImage(rs_elf_image)
+    elf_labels = []
+    reindeer_labels = []
 
-    
-    reindeer_photo = ImageTk.PhotoImage(rs_reindeer_image)
+    santa_label = tk.Label(santa_frame, image=santa_photo1)
+    santa_label.pack()
 
-    tk.Label(elves_frame, image=elf_photo).pack(side="top")
-    tk.Label(reindeers_frame, image=reindeer_photo).pack(side="top")
-    tk.Label(santa_frame, image=santa_photo).pack(side="top")
+    def update_window():
+        global elves_counter, reindeer_counter
 
-    """for _ in range(1):
-        tk.Label(elves_frame, image=elf_photo).pack(side="top")
-        tk.Label(reindeers_frame, image=reindeer_photo).pack(side="top")"""
+        for label in elf_labels:
+            label.destroy()
 
-    def add_elf():
-        elves_counter[0] += 1
-        tk.Label(elves_frame, image=elf_photo).pack(side="top")
-        window.after(1000, add_elf)
+        for _ in range(elves_counter):
+            elf_label = tk.Label(elves_frame, image=elf_photo)
+            elf_label.pack(side="top")
+            elf_labels.append(elf_label)
 
-    # Programar la adición periódica de elfos
-    window.after(1000, add_elf)
-    """""
-    # Mostrar imágenes en etiquetas
-    santa_label = tk.Label(window, image=santa_photo)
-    santa_label.pack(side = "left")
+        for label in reindeer_labels:
+            label.destroy()
 
-    elf_label = tk.Label(window, image=elf_photo)
-    elf_label.pack(side = "left")
+        for _ in range(reindeer_counter):
+            reindeer_label = tk.Label(reindeers_frame, image=reindeer_photo)
+            reindeer_label.pack(side="top")
+            reindeer_labels.append(reindeer_label)
 
-    reindeer_label = tk.Label(window, image=reindeer_photo)
-    reindeer_label.pack(side = "left")
-    """
+        if elves_counter == 3 or reindeer_counter == 9:
+            santa_label.config(image=santa_photo2)
+        elif elves_counter == 0 or reindeer_counter == 0:
+            santa_label.config(image=santa_photo1)
 
-    # Ejecutar programa en un hilo
+        window.after(1000, update_window)
+
+    update_window()
+
     program_thread = Thread(target=run_program)
     program_thread.start()
 
-    # Ejecutar bucle principal de la ventana
     window.mainloop()
-
 
 if __name__ == "__main__":
     main()
